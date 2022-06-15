3e0af3869a33f8a6cc9c04346f04af450fb8df68
Александр Мельников <AMelnikov@migcredit.ru>
2022-06-15 11:38:56 +0300

    task_2_model_work прописаны модели и админка

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teams, Student, Teacher
+
+
+@admin.register(Teams)
+class TeamsAdmin(admin.ModelAdmin):
+    """кафедра"""
+    search_fields = ('name',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    """Преподаватель"""
+    list_filter = ('teams_teacher',)
+    search_fields = ('first_name', 'middle_nam', 'last_name', 'teams_teacher')
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    """Студент"""
+
+    list_filter = ('teams_student',)
+    search_fields = ('first_name', 'middle_name', 'last_name', 'teams_student')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- study/models.py
+++ study/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Teams(models.Model):
+    """Кафедра"""
+    name = models.CharField(max_length=100, verbose_name='название кафедры')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'кафедра'
+        verbose_name_plural = 'кафедры'
+
+
+class Teacher(models.Model):
+    """Преподаватель"""
+    first_name = models.CharField(max_length=50, verbose_name='имя преподавателя')
+    middle_name = models.CharField(max_length=50, verbose_name='отчество')
+    last_name = models.CharField(max_length=50, verbose_name='фамилия')
+    teams_teacher = models.ForeignKey(Teams, verbose_name='кафедра', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.middle_name}'
+
+    class Meta:
+        verbose_name = 'преподаватель'
+        verbose_name_plural = 'преподаватели'
+
+
+class Student(models.Model):
+    """Студент"""
+    first_name = models.CharField(max_length=50, verbose_name='имя студента')
+    middle_name = models.CharField(max_length=50, verbose_name='отчество студента')
+    last_name = models.CharField(max_length=50, verbose_name='фамилия студента')
+    teams_student = models.ForeignKey(Teams, verbose_name='кафедра', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.middle_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
