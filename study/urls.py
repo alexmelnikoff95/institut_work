064a804a1179fedb547f9295ee51cd3c7a4ed82c
Александр Мельников <AMelnikov@migcredit.ru>
2022-06-28 12:44:06 +0300

    task_5 работа над api, реализована регистрация

--- study/urls.py
+++ study/urls.py
@@ -2,13 +2,15 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from study.views.views import IndexView, StudentListView, StudentDetailView
+from study.views.views_auth import RegisterUser
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('student-list', StudentListView.as_view(), name='student_list'),
     path('home-work/<int:id>', StudentDetailView.as_view(), name='home_work'),
 
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register', RegisterUser.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     # path('logout/', user_logout, name='logout'),
 ]
--- study/views/views_auth.py
+++ study/views/views_auth.py
@@ -1,39 +1,22 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
+
+from study.forms import UserRegisterForm
 
 
-# class LoginUser(LoginView):
-#     """Авторизация на сайте"""
-#     form_class = UserLoginForm
-#     template_name = 'login.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         return dict(list(context.items()))
-#
-#     def get_success_url(self) -> str:
-#         return reverse_lazy('index')
+class RegisterUser(CreateView):
+    """Регистрация пользователей"""
+    form_class = UserRegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('index')
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
 
-# class RegisterUser(CreateView):
-#     """Регистрация пользователей"""
-#     form_class = UserRegisterForm
-#     template_name = 'register.html'
-#     success_url = reverse_lazy('login')
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         return dict(list(context.items()))
-#
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return redirect('home')
-
-#
-# def user_logout(request):
-#     """Выход из профиля"""
-#     logout(request)
-#     return redirect('login')
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
